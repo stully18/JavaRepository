c922c88047bae55002c2a8de30f20cda0ccc2c2d
stully18 <stullyrl@gmail.com>
2024-10-07 19:31:54 -0400

    test push

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JF rame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Lara's Adventure");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+
+
+
+
+}
