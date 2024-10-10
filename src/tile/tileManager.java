e15da6bc17a8eba6eec8c51a8f310bc02519ddb2
stully18 <stullyrl@gmail.com>
2024-10-10 10:05:40 -0400

    new tiles

--- src/tile/tileManager.java
+++ src/tile/tileManager.java
@@ -33,6 +33,16 @@ public class tileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/water.png")));
 
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/earth.png")));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/sand.png")));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/tree.png")));
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
